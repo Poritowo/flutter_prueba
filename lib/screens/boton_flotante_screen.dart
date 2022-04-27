914093735d9bf35ccc04175ba0523fa87750ba29
FlutLab.io <>
2022-04-27 22:27:49 +0000

    Autosave

---
+++ lib/screens/boton_flotante_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Stless extends StatelessWidget {
+import'package:flutter/material.dart';
+  const Stless({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Boton Flotante"))
+      ),
+    );
+  }
+}
